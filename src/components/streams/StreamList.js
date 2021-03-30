baeb9dc9c1d43c88bc426098bc465aedd3f62a6f
sengdu <sengdu005416@gmail.com>
2021-03-30 17:10:26 +0630

    client-createStream

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,7 +1,56 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchStreams} from '../../actions';
 
-const StreamList = () => {
-  return <div>StreamList</div>;
-};
+class StreamList extends React.Component {
+  componentDidMount(){
+    this.props.fetchStreams();
+  }
+ 
+  renderAdmin(stream){
+    if(stream.userId === this.props.currentUserId){
+      return(
+        <div className=" right floated content">
+          <button className="ui button primary">
+            EDIT
+          </button>
+          <button className="ui button negative">
+            DELETE
+          </button>
+        </div>
+      )
+    }
+  }
 
-export default StreamList;
+
+  renderList (){
+    return this.props.streams.map(stream => {
+      return(
+        <div className="item" key={stream.id}>
+           {this.renderAdmin(stream)}
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      )
+    });
+  }
+  render(){
+    return (
+      <div>
+        <h2>streams</h2>
+        <div className="ui celled list">
+        {this.renderList()}
+        </div>
+      </div>
+    )
+  };
+}
+
+const mapStateToProps = (state) => {
+  return { streams: Object.values(state.streams),currentUserId:state.auth.userId};
+}
+
+export default connect(mapStateToProps,{fetchStreams}) (StreamList);
