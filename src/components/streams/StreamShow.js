41c0d0f9593835856b52de6e43369a4e87e9e31f
sengdu <sengdu005416@gmail.com>
2021-04-04 16:23:12 +0630

    stream-list-showing

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import flv from 'flv.js';
+import {connect} from 'react-redux';
+import {fetchStream} from '../../actions';
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+
+class StreamShow extends React.Component {
+  constructor(props){
+    super(props);
+    this.videoRef = React.createRef();
+  }
+  componentDidMount(){
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  render(){
+
+    if(! this.props.stream){
+      return <div>Loading...</div>
+    }
+    const {title,description} = this.props.stream;
+    return (
+      <div>
+        <video ref={this.videoRef} style={{ width: '100%'}} controls/>
+        <h1>{title}</h1>
+        <h3>{description}</h3>
+      </div>
+    )
+   }
 };
 
-export default StreamShow;
+const mapStateToProps = (state,ownProps) => {
+  return{stream: state.streams[ownProps.match.params.id]}
+}
+export default connect(mapStateToProps, {fetchStream}) (StreamShow);
